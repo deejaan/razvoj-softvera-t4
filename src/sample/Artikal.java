076fa5dcb0898744c28737042f1704f87c87a716
dejaan <dpopovic1@etf.unsa.ba>
2019-04-01 12:32:29 +0200

    Geteri seteri i toString() u klasi Artikal

--- src/sample/Artikal.java
+++ src/sample/Artikal.java
@@ -11,4 +11,34 @@ public class Artikal {
         cijena = Double.parseDouble(ulazni_podaci[2]);
     }
 
+    public String toString() {
+        return sifra + ',' + naziv + ',' + cijena;
+    }
+
+    public String getSifra() {
+        return sifra;
+    }
+
+    public void setSifra(String sifra) {
+        this.sifra = sifra;
+        if(sifra==null) throw new IllegalArgumentException();
+    }
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+        if(naziv==null) throw new IllegalArgumentException();
+    }
+
+    public double getCijena() {
+        return cijena;
+    }
+
+    public void setCijena(double cijena) {
+        this.cijena = cijena;
+        if(cijena <= 0) throw new IllegalArgumentException();
+    }
 }
