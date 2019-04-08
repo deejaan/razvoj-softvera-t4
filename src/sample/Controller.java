2b6f23113c873d5650347283857c17d682e1de2f
dejaan <dpopovic1@etf.unsa.ba>
2019-04-08 11:43:29 +0200

    zad2 i pocetak zad3, nova klasa racun

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,24 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextArea;
+
+import java.util.ArrayList;
+
 public class Controller {
+    public TextArea unos;
+    public TextArea rezultat;
+
+    private ArrayList<Artikal> artikli = new ArrayList<>();
+    public void dodajArtikle(ActionEvent actionEvent) {
+        String[] artikliStr = unos.getText().split("\n");
+
+        for(String linija: artikliStr) artikli.add(new Artikal(linija));
+        Artikal.izbaciDuplikate(artikli);
+
+        String res = "";
+        for(Artikal artikal: artikli) res += artikal + "\n";
+
+        rezultat.setText(res);
+    }
 }
